0d598f50d72f284a15743d5c6b2c25accec5105c
Tatsuro Shibamura <me@shibayan.jp>
2020-01-20 19:53:07 +0900

    Adding application insights

---
+++ modules/appInsights.tf
@@ -0,0 +1,6 @@
+resource "azurerm_application_insights" "default" {
+  name                = "${var.environment}-appservice-01"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  application_type    = "web"
+}
--- modules/appService.tf
+++ modules/appService.tf
@@ -3,6 +3,10 @@ resource "azurerm_app_service" "default" {
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   app_service_plan_id = azurerm_app_service_plan.default.id
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.default.instrumentation_key
+  }
 }
 
 resource "azurerm_app_service_virtual_network_swift_connection" "default" {
