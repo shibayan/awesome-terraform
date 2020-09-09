03f6061503207f6aa5c1008aa57ba73950bce5fa
Tatsuro Shibamura <me@shibayan.jp>
2020-09-09 21:31:10 +0900

    Update azure naming rule

--- modules/appInsights.tf
+++ modules/appInsights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_application_insights" "default" {
-  name                = "${var.environment}-appservice-01"
+  name                = "appi-appservice-${var.environment}"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   application_type    = "web"
--- modules/appService.tf
+++ modules/appService.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service" "default" {
-  name                = "${var.environment}-appservice-01"
+  name                = "app-appservice-${var.environment}"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   app_service_plan_id = azurerm_app_service_plan.default.id
