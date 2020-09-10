20d0cad6e08eea8d81fb10bb7180547e12de83e9
Tatsuro Shibamura <me@shibayan.jp>
2020-09-10 16:27:22 +0900

    Remove Microsoft.Web service endpoint

--- modules/virtualNetwork.tf
+++ modules/virtualNetwork.tf
@@ -10,7 +10,6 @@ resource "azurerm_subnet" "appservice" {
   resource_group_name  = azurerm_resource_group.default.name
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefixes     = ["10.0.0.0/24"]
-  service_endpoints    = ["Microsoft.Web"]
 
   delegation {
     name = "appservice_delegation"
